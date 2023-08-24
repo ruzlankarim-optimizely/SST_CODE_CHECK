c46fb6020ecd0928eaf891d4f393740eda05dd95
Farah Hamud Khan <110549139+farahkhan-optimizely@users.noreply.github.com>
2023-08-24 15:27:58 +0600

    New File Added for Drag Ratio -- Combo

---
+++ Cohort_1_2_Combo/Drag_Ratio_Combo/Drag_Ratio_Cohort1_Cohort2.sql
@@ -0,0 +1,42 @@
+--Code to create a combined drag ratio 
+
+with combined_drag_ration as 
+(
+SELECT 
+      mcid_arr,
+      "MAX Snapshot Date of TAT",
+      product_family_arr,
+      "Ratio of ARR Allocated to PF UFDM ARR" AS "Ratio of ARR",
+      "Date to Drag to Under Scenario 1" AS "Date to Drag: Sol. 1"
+FROM sandbox.cohort1_drag_ratio AS a 
+UNION ALL 
+SELECT 
+      mcid AS mcid_arr ,
+      "End of Drag Ratio in TAT" AS "Max Snapshot Date in TAT",
+      "Product Family in UFDM ARR" AS product_family_arr,
+      "Ratio of ARR for Each PF in UFDM ARR" AS "Ratio of ARR",
+      "Start of Drag Ratio in TAT" AS "Date to Drag: Sol. 1"
+FROM sandbox.cohort2_drag_ratio
+WHERE mcid NOT IN(SELECT  DISTINCT mcid_arr FROM sandbox.cohort1_drag_ratio)
+)
+
+select 
+	*
+from 
+	combined_drag_ration 
+
+--Tests 
+
+--select 
+--	distinct mcid
+--from 
+--	sandbox.cohort2_drag_ratio
+--where 
+--	mcid in 
+--		(
+--		SELECT  
+--			DISTINCT mcid_arr 
+--		FROM 
+--			sandbox.cohort1_drag_ratio
+--		)
+		
